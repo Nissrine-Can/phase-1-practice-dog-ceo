c2929695818a6b6e3ac716afeadb47b7ee8d0924
Nissrine Canina <niss.canina@gmail.com>
2021-09-23 10:06:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+ const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(dogData => dogData.message.forEach(dogUrl => {
+        let div = document.querySelector("#dog-image-container")
+       div.innerHTML = `
+       <img src = "${dogUrl}" />
+       `
+    }))
+   fetch("https://dog.ceo/api/breeds/list/all")
+    .then(res => res.json())
+    .then(function (breedData) {
+        const breeds = breedData.message
+        for (breedKey in breeds) {
+        const li = document.createElement('li')
+        li.textContent = breedKey
+        document.querySelector("#dog-breeds").appendChild(li)
+        li.style.cursor =  "pointer"
+
+        let lis = document.querySelectorAll("ul#dog-breeds > li")
+        console.log(lis)
+        lis.forEach(li => li.addEventListener("click", function() {
+            li.style.color = "red"
+        }))
+        
+        let option = document.getElementById("breed-dropdown")
+      //console.log(option)
+        //let select = document.querySelector("select#breed-dropdown > option")
+        // console.log(select)
+        
+        
+        
+        
+      }
+  })
+})
+  //dropdown
+  //let select = document.querySelector("#breed-dropdown")
+  //console.log(option)
+  
+  
+
+
+  
+  
+
+
+
